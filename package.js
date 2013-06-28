c7b59be254ed2682bee58949ab9964e05cf8f8e4
crapthings <crapthings@gmail.com>
2013-06-28 15:35:45 +0800

    fix err.

--- package.js
+++ package.js
@@ -1,9 +1,7 @@
 Package.describe({
-	summary: "A javascript date library for parsing, validating, manipulating, and formatting dates."
+  summary: "A javascript date library for parsing, validating, manipulating, and formatting dates."
 });
 
 Package.on_use(function(api) {
-	api.add_files([
-		'moment/moment.js'
-	], ['client', 'server']]);
+  api.add_files(['moment/moment.js'], ['client', 'server']);
 });
