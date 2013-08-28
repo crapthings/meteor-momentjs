9626c7a4f97b01b511b74ef1882d42708ae836d4
crapthings <crapthings@gmail.com>
2013-08-28 12:27:59 +0800

    fix tabs with two spaces, and make it work with 0.6.5

--- package.js
+++ package.js
@@ -2,6 +2,8 @@ Package.describe({
   summary: "A javascript date library for parsing, validating, manipulating, and formatting dates."
 });
 
-Package.on_use(function(api) {
-  api.add_files(['moment/min/moment.min.js', 'moment/min/langs.min.js'], ['client', 'server']);
+Package.on_use(function(api, where) {
+  where = where || ['client', 'server']
+  if (api.export) api.export('moment');
+  api.add_files(['moment/min/moment.min.js', 'moment/min/langs.min.js'], where);
 });
